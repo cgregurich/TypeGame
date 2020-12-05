2fd4df3ec9b46f10fc7d4b36bcdec8315e887ef5
Colin Gregurich <coling112198@gmail.com>
2020-12-05 00:32:31 -0600

    continued working on settings

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 from tkinter import *
 
 # WHERE I LEFT OFF:
-# getting the .ini and config shish up and running. currently making the func
-# init config and draw settings and shish
+# Got the editor saving to work (add ctrl+s bind??)
+# Had to fix the Text widget's default behavior of adding a '\n' to the end of the text
 
 # What do we want out of the settings?
 
@@ -10,6 +10,16 @@ from tkinter import *
 # Change the line count? 
 # Change the characters allowed per line?
 # access the list of words that are pulled (i.e. words.txt) Open this in a text widget?
+
+# TODO
+# - how to init editor in __init__ without it popping up???
+# - draw current file in Text widget
+# - have editor window pop up when button clicked
+# - only one instance of editor allowed
+# - check for changes when x'd out
+# - if main program is x'd, then close editor as well
+# - disable main program when editor is open
+
 
 from configmanager import ConfigManager
 
@@ -30,22 +40,26 @@ class Settings(Frame):
 		self.cfgmgr = ConfigManager()
 		self.draw_settings()
 
-
+		self.editor = None
+		self.editor_text = None
 
 		
-
-
 	def draw_settings(self):
 		settings = (("time", "time"), ("wordsfile", "words file"), ("linecount", "line count"), 
 						("maxcharsperline", "max chars per line"))
 		row = 0
 		for s in settings:
-			# draw a label and then an appropriate widget to modify the option
 			Label(self.options_frame, text=s[1].title(), font=FONT).grid(row=row, column=0)
-			e = Entry(self.options_frame, font=FONT)
-			e.grid(row=row, column=1)
-			self.entries[s[0]] = e
-			e.insert(0, self.cfgmgr.get_setting("SETTINGS", s[0]))
+			if s[0] == "wordsfile":
+				Button(self.options_frame, text="View/Edit", font=FONT, command=self.edit_file).grid(row=row, column=1)
+				
+			else:
+				# draw a label and then an appropriate widget to modify the option
+				
+				e = Entry(self.options_frame, font=FONT)
+				e.grid(row=row, column=1)
+				self.entries[s[0]] = e
+				e.insert(0, self.cfgmgr.get_setting("SETTINGS", s[0]))
 			row += 1
 		Button(self.options_frame, text="Save", font=FONT, command=self.save_clicked).grid(row=row, column=0, columnspan=2)
 
@@ -55,11 +69,29 @@ class Settings(Frame):
 	def save_settings(self):
 		for name, entry in self.entries.items():
 			self.cfgmgr.change_setting("SETTINGS", name.title(), entry.get())
-			print(f"change setting {name}")
-
-		
-
 
 	def back_clicked(self):
 		self.controller.show_frame("TypeGame")
 
+	def edit_file(self):
+		self.editor = Toplevel()
+		self.editor_text = Text(self.editor, font=FONT)
+		self.editor_text.grid(row=0, column=0)
+		button_frame = Frame(self.editor)
+		button_frame.grid(row=1, column=0)
+		Button(button_frame, text="Save", font=FONT, command=self.save_editor).grid(row=0, column=0)
+
+		
+
+		with open("resources/words.txt", "r") as file:
+			words = file.read()
+		
+		
+		self.editor_text.insert(INSERT, words)
+
+	def save_editor(self):
+		contents = self.editor_text.get("0.0", END)
+		print(contents)
+
+		with open("resources/words.txt", "w") as file:
+			file.write(contents[:len(contents)-1])
